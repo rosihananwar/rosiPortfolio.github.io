551ef3eb254a18fc24447e39c59bec8bd2ee7caf
rosihananwar <rosi.mandala@gmail.com>
2021-08-25 19:08:21 +0700

    update kedua

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ $(document).ready(function () {
     }
   });
 });
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbzayjYpC0Io9Qm24p5Kn9Tnd3Lerlr9tfkZXCCi7_h1EU8eUAE5-QDHWoZ7ei4emiXAWA/exec";
+const form = document.forms["contactForm"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
